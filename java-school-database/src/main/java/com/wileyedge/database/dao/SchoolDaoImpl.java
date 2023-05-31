d954fd0efb9499bec762bcc60f6d60e834b32ccc
Pary <81phary@gmail.com>
2023-05-31 17:11:07 +1000

    edit incorrect spelling of query to delete teacher

--- java-school-database/src/main/java/com/wileyedge/database/dao/SchoolDaoImpl.java
+++ java-school-database/src/main/java/com/wileyedge/database/dao/SchoolDaoImpl.java
@@ -121,7 +121,7 @@ public class SchoolDaoImpl implements SchoolDao {
         // Write a query to remove David Mitchell as a teacher.
         // YOUR CODE STARTS HERE
 
-        String sql = "DELETE FROM teacher WHERE fFName = 'David' AND tLName='Mitchell' ";
+        String sql = "DELETE FROM teacher WHERE tFName = 'David' AND tLName='Mitchell' ";
 
         // YOUR CODE ENDS HERE
         jdbcTemplate.update(sql);
